4ae3e4ed2606f8e0f7bc60fa69f7cb3dbc513079
Mitoshima <umbe0913@gmail.com>
2025-02-23 15:00:15 +0900

    whoIsWinner

--- Temp.java
+++ Temp.java
@@ -1,7 +1,24 @@
-package Java;
+package JavaLearning;
 
 public class Temp {
-    public static void main(String[] arg) {
-        System.out.println("Hello World");
+    public static String whoIsWinner(int hisCard){
+        // 関数を完成させてください
+        final int myCard = 10;
+        if (hisCard == 2 || hisCard == 1 || hisCard > myCard) {
+            return "you lose";
+        } else if (hisCard == myCard) {
+            return "draw";
+        } else {
+            return "you win";
+        }
+    }
+
+    public static void main(String[] args){
+        System.out.println(whoIsWinner(1));
+        System.out.println(whoIsWinner(2));
+        System.out.println(whoIsWinner(3));
+        System.out.println(whoIsWinner(9));
+        System.out.println(whoIsWinner(10));
+        System.out.println(whoIsWinner(11));
     }
 }
