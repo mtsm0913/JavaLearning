8b55a94f5d125c7c42bb58108ba41b562864539d
Mitoshima <umbe0913@gmail.com>
2025-03-05 23:12:48 +0900

    Output for 2025/03/05

--- IsValidEmail.java
+++ IsValidEmail.java
@@ -1,36 +1,53 @@
-package JavaLearning;
+    package JavaLearning;
 
-/*
- * あるメールアドレス email を受け取るので、それが正しいものであれば true を、
- * そうでなければ false を返す isValidEmail という関数を作成してください。
- * ここでの正しいアドレスというのは以下の 4 つの条件を満たすものを指します。
- * ・「@」から始まらないこと
- * ・スペースがないこと
- * ・「@」を 1 つのみ含んでいること
- * ・「@」の後に「.」があること
- */
-public class IsValidEmail {
-    public static boolean isValidEmail(String email) {
-        if (email.indexOf("@") <= 0) {
-            return false;
-        } else if (email.indexOf(" ") < 0) {
-            return false;
-        } else if (email.indexOf("@") > 0) {
-            if (email.substring(email.indexOf("@") + 1, email.length()).indexOf("@") >= 0) {
-                return false;
-            } else if (email.substring(email.indexOf("@") + 1, email.length()).indexOf(".") < 0) {
+    /*
+    * あるメールアドレス email を受け取るので、それが正しいものであれば true を、
+    * そうでなければ false を返す isValidEmail という関数を作成してください。
+    * ここでの正しいアドレスというのは以下の 4 つの条件を満たすものを指します。
+    * ・「@」から始まらないこと
+    * ・スペースがないこと
+    * ・「@」を 1 つのみ含んでいること
+    * ・「@」の後に「.」があること
+    */
+
+    public class IsValidEmail {
+        public static boolean isValidEmail(String email) {
+
+            // "@"のindexが0 -> false
+            if (email.indexOf("@") <= 0) {
                 return false;
             }
+
+            // " "がある -> false
+            if (email.indexOf(" ") >= 0) {
+                return false;
+            }
+
+            // "@"が複数存在する -> false
+            if (email.indexOf("@", email.indexOf("@") + 1) != -1) {
+                return false;
+            }
+            // 別解
+            // if (email.indexOf("@") != email.lastIndexOf("@")) {
+            //     return false;
+            // }
+
+            // "@"の後ろに"."が存在しない -> false
+            if (email.indexOf(".", email.indexOf("@")) == -1) {
+                return false;
+            }
+
+            // "@"のindexが0 かつ "@"が1つだけ存在し、"@"の後ろに"."が存在しない -> true
             return true;
         }
+
+        public static void main(String[] args) {
+            System.out.println(isValidEmail("ccc@aaa.com"));
+            System.out.println(isValidEmail("c@cc@aaa.com"));
+            System.out.println(isValidEmail("cc c@aaa.com"));
+            System.out.println(isValidEmail("cc.c@aaacom"));
+            System.out.println(isValidEmail("cc.c@aaa.com"));
+            System.out.println(isValidEmail("@aaa.com"));
+            System.out.println(isValidEmail("aaaccc.com"));
+        }
     }
-    public static void main(String[] args) {
-        System.out.println(isValidEmail("ccc@aaa.com"));
-        // System.out.println(isValidEmail(""));
-        // System.out.println(isValidEmail(""));
-        // System.out.println(isValidEmail(""));
-        // System.out.println(isValidEmail(""));
-        // System.out.println(isValidEmail(""));
-        // System.out.println(isValidEmail(""));
-    }
-}
