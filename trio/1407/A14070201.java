f5d249fba40199dc6cc6d638307166b75dd17df4
mtsm0913 <umbe0913@gmail.com>
2026-01-29 16:33:27 +0900

    202601291633

---
+++ trio/1407/A14070201.java
@@ -0,0 +1,13 @@
+/*
+問題1：1〜5を順番に表示
+条件
+ for文で1〜5を表示する
+*/
+
+class A14070201 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
