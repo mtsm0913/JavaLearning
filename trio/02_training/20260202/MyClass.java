eb3b6d72b692a1b9f83b5c1a623bb52d6f720573
mtsm0913 <umbe0913@gmail.com>
2026-02-02 14:02:35 +0900

    20260202_Add Training

---
+++ trio/02_training/20260202/MyClass.java
@@ -0,0 +1,16 @@
+class MyClass {
+    // Class：プログラムのまとまり
+
+    public static void main(String[] args) {
+        // Javaプログラムのmainという入り口
+        
+        System.out.println("太田です。");
+        System.out.println("こんにちは");
+        /*
+        ln：lineの略
+        println()：画面に文字を表示する命令
+        println("")のダブルクォーテーションの中に
+        表示したい文字を入れる。
+        */
+    }
+}
