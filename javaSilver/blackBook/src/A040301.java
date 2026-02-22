fb78b27c4d47ad2aebd0448a8b20f5be5e5d6adf
Mitoshima <umbe0913@gmail.com>
2026-02-22 09:05:32 +0900

    202602220905

---
+++ javaSilver/blackBook/src/A040301.java
@@ -0,0 +1,19 @@
+//class A040301 {
+//    public static void main(String[] args) {
+//        int cnt = 0;
+//        while(cnt++ < 5)
+////        while(++cnt < 5)
+//            System.out.println("A");
+//            System.out.println("B");
+//    }
+//}
+
+class A040301 {
+    public static void main(String[] args) {
+        int cnt = 0;
+        while(cnt++ < 5) {
+            System.out.println("A");
+        }
+        System.out.println("B");
+    }
+}
---
+++ javaSilver/blackBook/src/A120501.java
@@ -0,0 +1,8 @@
+public class A120501 {
+    static int num = 23;
+    public static void main(String[] args) {
+//        int num = num;
+        int num = A120501.num;
+        System.out.println(num);
+    }
+}
