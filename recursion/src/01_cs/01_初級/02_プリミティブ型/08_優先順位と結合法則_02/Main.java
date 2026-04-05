f50aac856ac6ca0fb45e9cae96a3230378ea9542
Mitoshima <umbe0913@gmail.com>
2026-04-05 13:30:51 +0900

    のサンプルコード

---
+++ recursion/src/01_cs/01_初級/02_プリミティブ型/08_優先順位と結合法則_02/Main.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args){
+        System.out.println(2 + 4 - 5);
+        System.out.println(10 - 2 * 5);
+        System.out.println(14 + 10 / 5 * 4 - 1);
+    }
+}
